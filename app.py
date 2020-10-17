ae7a0eace27a693663869b1022db395344f8471a
Rohan Poudel <rohan-poudel@github.com>
2020-10-17 21:23:05 +0545

    added index route

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ app = Flask(__name__)
 
 
 @app.route("/rohan", methods=["GET", "POST"])
+def rohan():
+    return "<h1> hello rohan </h1>"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h1> hello </h1>"
+    return "<h1> hello world </h1>"
+
+
 
 app.run(debug=True)
+
 
 
